c4713d2797c6d67a2d8a67a12b9be32319cda198
jones-semicolon <marjones.lapinig@gmail.com>
2025-06-25 03:37:22 +0800

    cleaned

--- .config/nvim/lua/jones/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/jones/plugins/lsp/lspconfig.lua
@@ -1,7 +1,7 @@
 -- import lspconfig plugin safely
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-  return
+	return
 end
 
 require("neodev").setup({})
@@ -13,7 +13,7 @@ require("neodev").setup({})
 -- import cmp-nvim-lsp plugin safely
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
-  return
+	return
 end
 
 -- import typescript plugin safely
@@ -24,44 +24,44 @@ end
 
 local lsp_signature_status, lsp_signature = pcall(require, "lsp_signature")
 if not lsp_signature_status then
-  print("no lsp-signature")
-  return
+	print("no lsp-signature")
+	return
 end
 
 local keymap = vim.keymap -- for conciseness
 
 -- enable keybinds only for when lsp server available
 local on_attach = function(client, bufnr)
-  -- keybind options
-  local opts = { noremap = true, silent = true, buffer = bufnr }
+	-- keybind options
+	local opts = { noremap = true, silent = true, buffer = bufnr }
 
-  -- set keybinds
-  keymap.set("n", "gf", "<cmd>Lspsaga finder<CR>", opts)                         -- show definition, references
-  keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)          -- got to declaration
-  keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)                -- see definition and make edits in window
-  keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)       -- go to implementation
-  keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)            -- see available code actions
-  keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)                 -- smart rename
-  keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)   -- show  diagnostics for line
-  keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
-  keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)           -- jump to previous diagnostic in buffer
-  keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)           -- jump to next diagnostic in buffer
-  keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)                       -- show documentation for what is under cursor
-  -- keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+	-- set keybinds
+	keymap.set("n", "gf", "<cmd>Lspsaga finder<CR>", opts) -- show definition, references
+	keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+	keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+	keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+	keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+	-- keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
 
-  -- typescript specific keymaps (e.g. rename file and update imports)
-  if client.name == "ts_ls" then
-    keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>")      -- rename file and update imports
-    keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
-    keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>")    -- remove unused variables (not in youtube nvim video)
-  end
+	-- typescript specific keymaps (e.g. rename file and update imports)
+	if client.name == "ts_ls" then
+		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
+		keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
+		keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
+	end
 
-  lsp_signature.on_attach({
-    bind = true, -- This is mandatory, otherwise border config won't get registered.
-    handler_opts = {
-      border = "rounded",
-    },
-  }, bufnr)
+	lsp_signature.on_attach({
+		bind = true, -- This is mandatory, otherwise border config won't get registered.
+		handler_opts = {
+			border = "rounded",
+		},
+	}, bufnr)
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
@@ -69,8 +69,8 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
 }
 
 local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -81,49 +81,49 @@ capabilities = vim.tbl_deep_extend("force", capabilities, cmp_capabilities)
 -- (not in youtube nvim video)
 local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 local language_servers = lspconfig.util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
 for _, ls in ipairs(language_servers) do
-  lspconfig[ls].setup({
-    capabilities = capabilities,
-    -- you can add other fields for setting up lsp server in this table
-    on_attach = on_attach,
-  })
+	lspconfig[ls].setup({
+		capabilities = capabilities,
+		-- you can add other fields for setting up lsp server in this table
+		on_attach = on_attach,
+	})
 end
 
 -- configure html server
 lspconfig["html"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
-lspconfig["pylsp"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  filetypes = {
-    "python",
-    "kivy"
-  },
-  settings = {
-    pylsp = {
-      plugins = {
-        -- Enable pylint
-        pylint = { enabled = true },
-        -- Disable other linters
-        flake8 = { enabled = false },
-        mypy = { enabled = false },
-        pycodestyle = { enabled = false },
-        pydocstyle = { enabled = false },
-        pyflakes = { enabled = false },
-        pyls_mypy = { enabled = false },
-        pyls_isort = { enabled = false },
-        mccabe = { enabled = false },
-        autopep8 = { enabled = false },
-      },
-    },
-  },
+lspconfig["pyright"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = {
+		"python",
+		"kivy",
+	},
+	settings = {
+		pylsp = {
+			plugins = {
+				-- Enable pylint
+				pylint = { enabled = true },
+				-- Disable other linters
+				flake8 = { enabled = false },
+				mypy = { enabled = false },
+				pycodestyle = { enabled = false },
+				pydocstyle = { enabled = false },
+				pyflakes = { enabled = false },
+				pyls_mypy = { enabled = false },
+				pyls_isort = { enabled = false },
+				mccabe = { enabled = false },
+				autopep8 = { enabled = false },
+			},
+		},
+	},
 })
 -- configure typescript server with plugin
 -- typescript.setup({
@@ -135,27 +135,27 @@ lspconfig["pylsp"].setup({
 
 -- configure css server
 lspconfig["cssls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  filetypes = {
-    "css",
-  },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = {
+		"css",
+	},
 })
 
 lspconfig.eslint.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 lspconfig.ts_ls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 -- configure tailwindcss server
 lspconfig["tailwindcss"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 -- configure kotlin server
 -- lspconfig["kotlin_language_server"].setup({
@@ -164,70 +164,70 @@ lspconfig["tailwindcss"].setup({
 -- })
 
 lspconfig["vls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 -- configure java server
 lspconfig["jdtls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 -- configure emmet language server
 lspconfig["emmet_ls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  filetypes = {
-    "html",
-    "typescriptreact",
-    "javascriptreact",
-    -- "css",
-    "sass",
-    "scss",
-    "less",
-    "svelte",
-    "kotlin",
-    "java",
-    "vue",
-  },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = {
+		"html",
+		"typescriptreact",
+		"javascriptreact",
+		-- "css",
+		"sass",
+		"scss",
+		"less",
+		"svelte",
+		"kotlin",
+		"java",
+		"vue",
+	},
 })
 
 if OS == "termux" then
-  lspconfig["lua_ls"].setup({
-    cmd = {
-      "/data/data/com.termux/files/usr/share/lua-language-server/bin/lua-language-server",
-      "-E",
-      "/data/data/com.termux/files/usr/share/lua-language-server/bin/main.lua",
-    },
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = { -- custom settings for lua
-      Lua = {
-        -- make the language server recognize "vim" global
-        runtime = {
-          version = "LuaJIT",
-          path = vim.split(package.path, ";"),
-        },
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          -- make language server aware of runtime files
-          library = {
-            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-            [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-            [vim.fn.stdpath("config") .. "/lua"] = true,
-          },
-        },
-      },
-    },
-  })
+	lspconfig["lua_ls"].setup({
+		cmd = {
+			"/data/data/com.termux/files/usr/share/lua-language-server/bin/lua-language-server",
+			"-E",
+			"/data/data/com.termux/files/usr/share/lua-language-server/bin/main.lua",
+		},
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = { -- custom settings for lua
+			Lua = {
+				-- make the language server recognize "vim" global
+				runtime = {
+					version = "LuaJIT",
+					path = vim.split(package.path, ";"),
+				},
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make language server aware of runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
 elseif OS == "windows" then
-  lspconfig["lua_ls"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-  })
+	lspconfig["lua_ls"].setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+	})
 else
-  print("Unknown terminal")
+	print("Unknown terminal")
 end
--- .config/nvim/lua/jones/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/jones/plugins/lsp/null-ls.lua
@@ -1,39 +1,39 @@
 local async_formatting = function(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-  if vim.b.formatting then
-    return
-  end
-  vim.b.formatting = true
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
+	if vim.b.formatting then
+		return
+	end
+	vim.b.formatting = true
 
-  vim.lsp.buf_request(
-    bufnr,
-    "textDocument/formatting",
-    vim.lsp.util.make_formatting_params({}),
-    function(err, res, ctx)
-      -- print(err)
-      vim.b.formatting = false
-      if err then
-        -- local err_msg = type(err) == "string" and err or err.message
-        -- you can modify the log message / level (or ignore it completely)
-        -- vim.notify("formatting error: " .. err_msg, vim.log.levels.ERROR)
-        return
-      end
+	vim.lsp.buf_request(
+		bufnr,
+		"textDocument/formatting",
+		vim.lsp.util.make_formatting_params({}),
+		function(err, res, ctx)
+			-- print(err)
+			vim.b.formatting = false
+			if err then
+				-- local err_msg = type(err) == "string" and err or err.message
+				-- you can modify the log message / level (or ignore it completely)
+				-- vim.notify("formatting error: " .. err_msg, vim.log.levels.ERROR)
+				return
+			end
 
-      -- don't apply results if buffer is unloaded or has been modified
-      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-        return
-      end
+			-- don't apply results if buffer is unloaded or has been modified
+			if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+				return
+			end
 
-      if res then
-        local client = vim.lsp.get_client_by_id(ctx.client_id)
-        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-        vim.api.nvim_buf_call(bufnr, function()
-          vim.cmd("silent! noautocmd update")
-        end)
-        -- vim.notify(vim.bo.filetype .. " formatted", vim.log.levels.INFO)
-      end
-    end
-  )
+			if res then
+				local client = vim.lsp.get_client_by_id(ctx.client_id)
+				vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+				vim.api.nvim_buf_call(bufnr, function()
+					vim.cmd("silent! noautocmd update")
+				end)
+				-- vim.notify(vim.bo.filetype .. " formatted", vim.log.levels.INFO)
+			end
+		end
+	)
 end
 --
 -- -- to setup format on save
@@ -42,8 +42,8 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 -- import null-ls plugin safely
 local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-  print("null ls not installed")
-  return
+	print("null ls not installed")
+	return
 end
 
 -- local none_ls = require("none-ls")
@@ -53,79 +53,82 @@ local helpers = require("null-ls.helpers")
 
 -- create a custom source that runs shellcheck and returns diagnostics
 local shellcheck = helpers.make_builtin({
-  method = null_ls.methods.DIAGNOSTICS,
-  filetypes = { "sh" },
-  generator_opts = {
-    command = "shellcheck",
-    to_stdin = true,
-    format = "json",
-    check_exit_code = function(code)
-      return code <= 1
-    end,
-    on_output = function(params)
-      local diagnostics = {}
-      for _, diagnostic in ipairs(params.output) do
-        table.insert(diagnostics, {
-          row = diagnostic.line,
-          col = diagnostic.column,
-          message = diagnostic.message,
-          severity = diagnostic.level,
-          source = "shellcheck",
-        })
-      end
-      return diagnostics
-    end,
-  },
-  factory = helpers.generator_factory,
+	method = null_ls.methods.DIAGNOSTICS,
+	filetypes = { "sh" },
+	generator_opts = {
+		command = "shellcheck",
+		to_stdin = true,
+		format = "json",
+		check_exit_code = function(code)
+			return code <= 1
+		end,
+		on_output = function(params)
+			local diagnostics = {}
+			for _, diagnostic in ipairs(params.output) do
+				table.insert(diagnostics, {
+					row = diagnostic.line,
+					col = diagnostic.column,
+					message = diagnostic.message,
+					severity = diagnostic.level,
+					source = "shellcheck",
+				})
+			end
+			return diagnostics
+		end,
+	},
+	factory = helpers.generator_factory,
 })
 
 -- register sources
 null_ls.register(shellcheck)
 null_ls.register(null_ls.builtins.formatting.prettier.with({
-  disabled_filetypes = {
-    "typescriptreact",
-    "vue",
-    "lua",
-  },
+	disabled_filetypes = {
+		"typescriptreact",
+		"vue",
+		"lua",
+	},
 }))
 null_ls.register(null_ls.builtins.formatting.ktlint.with({
-  filetypes = { "kotlin", "kt", "kts" },
+	filetypes = { "kotlin", "kt", "kts" },
 }))
 null_ls.register(null_ls.builtins.formatting.stylua.with({
-  filetypes = { "lua" },
+	filetypes = { "lua" },
 }))
 null_ls.register(null_ls.builtins.diagnostics.eslint_d.with({
-  condition = function(utils)
-    return utils.root_has_file(".eslintrc.js")
-  end,
+	condition = function(utils)
+		return utils.root_has_file(".eslintrc.js")
+	end,
 }))
 null_ls.register(null_ls.builtins.formatting.black.with({
-  filetypes = { "python" },
+	filetypes = { "python" },
 }))
--- null_ls.register(null_ls.builtins.diagnostics.pylint.with({
---   filetypes = { "python" },
--- }))
+null_ls.register(null_ls.builtins.diagnostics.mypy.with({
+	filetypes = { "python" },
+}))
+null_ls.register(null_ls.builtins.diagnostics.ruff.with({
+	filetypes = { "python" },
+}))
 
 -- configure null-ls
 null_ls.setup({
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- vim.lsp.buf.format({
-          --   filter = function(client)
-          --     return client.name == "null-ls"
-          --     -- return client.name == vim.bo.filetype
-          --   end,
-          --   bufnr = bufnr,
-          --   async = true,
-          -- })
-          async_formatting(bufnr)
-        end,
-      })
-    end
-  end,
+	on_attach = function(current_client, bufnr)
+		if current_client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- vim.lsp.buf.format({
+					--   filter = function(client)
+					--     return client.name == "null-ls"
+					--     -- return client.name == vim.bo.filetype
+					--   end,
+					--   bufnr = bufnr,
+					--   async = true,
+					-- })
+					async_formatting(bufnr)
+				end,
+			})
+		end
+	end,
 })
